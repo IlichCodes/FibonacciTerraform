9f440effbd5a0b57602d0035f4f6c9c33b6929cb
Sergei Litvinov <ext_litvinos@lloyds.com>
2021-10-06 14:51:39 +0100

    logic updated

--- Source/FindFibonacci.cs
+++ Source/FindFibonacci.cs
@@ -14,7 +14,7 @@ namespace Company.Function
 {
     public static class FindFibonacci
     {
-        [FunctionName("FindFibonacci11")]
+        [FunctionName("FindFibonacci")]
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
@@ -22,19 +22,21 @@ namespace Company.Function
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+           int firstNumber = 0;
+            int secondNumber = 1;
+            int nextFibonacciNo = firstNumber + secondNumber;
+            while (nextFibonacciNo <= inputNumber)
+            {
+                firstNumber = secondNumber;
+                secondNumber = nextFibonacciNo;
+                nextFibonacciNo = firstNumber + nextFibonacciNo;
+            }
+            int closestNumber = nextFibonacciNo - inputNumber < inputNumber - secondNumber ? nextFibonacciNo : secondNumber;
+            if (closestNumber == secondNumber)
+            {
+                return new OkObjectResult(new int[] { closestNumber, firstNumber, nextFibonacciNo });               
+            }
+            return new OkObjectResult(new int[] { closestNumber, secondNumber, nextFibonacciNo + secondNumber}); 
         }
     }
 }
